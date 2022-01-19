d4780b2c3ce2a64f7de9eb889bf36da5decb08d8
Abhijit Kumar Singh <thisabhijithere@gmail.com>
2022-01-20 01:00:53 +0530

    Added Public and Private Key Protocol. It would be much easier to createkey pairs and then encrypt the private key with a passphrase. I am not sure if this is for the best, but I think I would store encrypted private keys in the environment. Later I would probably use AER to encrypt the private key and do the changes. Current changes include actually making the .sql file so that the database can be created without manually setting up each table in the database. I am also thinking of automizing the mysqldump, tho that is yet to be implemented.

--- Create.py
+++ Create.py
@@ -3,16 +3,49 @@ from Crypto.Hash import keccak
 from TFA import generate_secret
 class People:
     def __init__(self, first_name, middle_name, last_name, address,email,dob, phone):
+        
+        def generate_keypair():
+            from Crypto.PublicKey import RSA
+            import pickle
+            key = RSA.generate(2048)
+            private_key = key.export_key()
+            file_private = open("private.pem", "wb")
+            file_private.write(private_key)
+            file_private.close()
+            file_private = open("private.pem","rb")
+            public_key = key.publickey().export_key()
+            file_public = open("receiver.pem", "wb")
+            file_public.write(public_key)
+            file_public.close()
+            file_public = open("receiver.pem", "rb")
+            public_key = pickle.load(file_public)
+            private_key = pickle.load(file_private)
+            return public_key,private_key
+            
+        
         import TFA
         from datetime import datetime
         key = hash(str((datetime.now().timestamp())))
         token = generate_secret()
-        from mysql import connector
+        from mariadb import connector
         t = connector.connect(user="python",passwd="Python",database="Bank", host="localhost")
         c = t.cursor()
         c.execute("select * from uuid()")
         uuid = c.fetchone()
-        sql = '''INSERT INTO PEOPLE (UNIQUE_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, ADDRESS, PHONE_NUMBER, EMAIL, DOB, SECURITY_NUMBER, 2FA_TOKEN_ID) VALUES (uuid, first_name, middle_name, last_name, address, phone, email, dob, num, tok)'''.format(first_name=first_name, middle_name=middle_name, last_name=last_name, address=address, email=email, phone=phone, dob=dob, security_number=key, tok=token, uuid = uuid)
+        from Hashing import sha256
+        uuid = sha256(uuid)
+        publickey, privatekey = generate_keypair()
+        sql = '''INSERT INTO `People` 
+        (`ID`, `FirstName`, `MiddleName`, `LastName`, `DateOfBirth`, `Address`, `Phone`, `Email`, `PublicKey`)
+        VALUES (uuid, first_name, middle_name, last_name, address, phone, email, public_key)'''.format(
+            uuid=uuid,
+            first_name=first_name,
+            middle_name=middle_name,
+            last_name=last_name,
+            address=address,
+            phone=phone,
+            email=email,
+            public_key=publickey)
         t.commit()
         c.execute(sql)
         
